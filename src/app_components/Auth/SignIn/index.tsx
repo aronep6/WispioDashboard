5eea8e94f95e615bd9ab5f67b60e4bf0452bef10
Ousmane Mbaye <mbayeousmane900@gmail.com>
2023-03-04 17:06:10 +0100

    feat(auth): Type added for the SginIn page + Removed ServiceSession context

--- src/app_components/Auth/SignIn/index.tsx
+++ src/app_components/Auth/SignIn/index.tsx
@@ -1,13 +1,16 @@
-import { SecondaryButton, SubmitPrimaryButton } from "../../atomic/Button";
+import { SecondaryButton, SubmitPrimaryButton } from "../../../app_atomic/Button";
 import { useEffect, useState } from "react";
 
-import AuthWrapper from "./layout";
+import AuthWrapper from "../layout";
 import { Link, useNavigate } from 'react-router-dom';
-import { useService, useUserSession } from '../../hooks';
-import { InputAsSelector } from '../../atomic/Input';
+
+import useUserSession from "../../../app_hooks/contexts_hooks/useUserSession";
+import useAuth from "../../../app_hooks/contexts_hooks/useAuth";
+
+import { InputBlock } from "../../../app_atomic/Input";
 
 // Form validation schema and deps
-import { getErrors, signInValidationSchema } from './functions';
+import { getErrors, signInValidationSchema } from '../functions';
 import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 
@@ -16,11 +19,11 @@ const inDev = !import.meta.env.PROD;
 const Login = () => {
     let navigate = useNavigate();
 
-    const service = useService();
+    const auth = useAuth();
     const user = useUserSession();
 
     const [isLoading, setIsLoading] = useState(false);
-    const [globalError, setGlobalError] = useState(null);
+    const [globalError, setGlobalError] = useState<string | null>(null);
 
     useEffect(() => {
         // Checking URL params
@@ -34,34 +37,34 @@ const Login = () => {
         }
     }, []);
 
-    const checkForRegistrationRedirection = async (uid) => {
-        try {
-            const { claims } = await service.getCustomClaims();
+    // const checkForRegistrationRedirection = async (uid: string) => {
+    //     try {
+    //         const { claims } = await service.getCustomClaims();
 
-            if (!claims) throw 'No custom claims found for this user';
-            // Check if the user need to add professional email
-            const params = new URLSearchParams(window.location.search);
+    //         if (!claims) throw 'No custom claims found for this user';
+    //         // Check if the user need to add professional email
+    //         const params = new URLSearchParams(window.location.search);
 
-            const flow = params.get('flow');
+    //         const flow = params.get('flow');
             
-            if (flow === 'addProfessionnalEmail' && claims.userIsReadyToUse) {
-                return navigate('/services/emails/create');
-            }
-            if (claims.userIsReadyToUse) return navigate('/app/dashboard');
-            throw 'User is not ready to use';
-        } catch (err) {
-            inDev && console.log("Une erreur est survenue lors de la vérification de l'utilisateur connecté : ", err);
-            // Redirect to the registration page
-            navigate(`/auth/checkup?id=${uid}&isImperative=true&fallback=default&flow=default`);
-            return;
-        }
-    };
+    //         if (flow === 'addProfessionnalEmail' && claims.userIsReadyToUse) {
+    //             return navigate('/services/emails/create');
+    //         }
+    //         if (claims.userIsReadyToUse) return navigate('/app/dashboard');
+    //         throw 'User is not ready to use';
+    //     } catch (err) {
+    //         inDev && console.log("Une erreur est survenue lors de la vérification de l'utilisateur connecté : ", err);
+    //         // Redirect to the registration page
+    //         navigate(`/auth/checkup?id=${uid}&isImperative=true&fallback=default&flow=default`);
+    //         return;
+    //     }
+    // };
 
     useEffect(() => {
-        if (user === null) return;
-        if (user === undefined) return;
+        if (user === null || user === undefined) return;
         // Checking if user is already connected
-        checkForRegistrationRedirection(user.uid);
+        console.log("Need to check for registration redirection (Aborted) : Not implemented yet :( ")
+        // checkForRegistrationRedirection(user.uid);
     }, [user]);
 
     // Handle form submission dans datas
@@ -69,40 +72,17 @@ const Login = () => {
         resolver: yupResolver(signInValidationSchema)
     });
 
-    /*
-        // Auto focus on email input
-        useEffect(() => {
-            setFocus("signin_form_email")
-        }, [ setFocus ]);
-    */
-
-    const handleFormLogin = async (data) => {
+    const handleFormLogin = async (data: any) => {
         try {
             setIsLoading(true);
-            const _user_ = await service.loginWithEmail(data.signin_form_email, data.signin_form_password);
+            const _user_ = await auth.loginWithEmail(data.signin_form_email, data.signin_form_password);
             setGlobalError(null);
-            checkForRegistrationRedirection(_user_.user.uid);
+            console.log("Need to check for registration redirection (Aborted) : Not implemented yet :( ")
+            // checkForRegistrationRedirection(_user_.user.uid);
             // Check if the user 
-        } catch (err) {
+        } catch (err: Error | unknown) {
             inDev && console.log("Une erreur est survenue lors de la connexion de l'utilisateur : ", err);
-            const _err = getErrors(err.message);
-            setGlobalError(_err);
-        } finally {
-            setIsLoading(false);
-        }
-    };
-
-    /* ========== OR LOGIN WITH AUTH PROVIDER ========= */
-
-    const handleProviderLogin = async (provider) => {
-        setIsLoading(true);
-        try {
-            const _user_ = await service.loginWithProvider(provider);
-            setGlobalError(null);
-            checkForRegistrationRedirection(_user_.user.uid);
-        } catch (error) {
-            inDev && console.log("Une erreur est survenue lors de la connexion avec le service : ", error);
-            const _err = getErrors(error.message);
+            const _err = getErrors(err);
             setGlobalError(_err);
         } finally {
             setIsLoading(false);
@@ -110,7 +90,7 @@ const Login = () => {
     };
 
     return <AuthWrapper 
-        title="Se connecter à Seendy"
+        title="Se connecter à Wispio"
         description="Connectez-vous à votre compte pour accéder à vos services"
         isLoading={ isLoading }
         returnLink="/"
@@ -126,7 +106,7 @@ const Login = () => {
                     field: { onChange, value },
                     fieldState: { error },
                 }) => (
-                    <InputAsSelector
+                    <InputBlock
                         name="signin_form_email"
                         label="Adresse e-mail"
                         placeholder="Adresse e-mail"
@@ -146,7 +126,7 @@ const Login = () => {
                     field: { onChange, value },
                     fieldState: { error },
                 }) => (
-                    <InputAsSelector
+                    <InputBlock
                         name="signin_form_password"
                         label="Mot de passe"
                         placeholder="Mot de passe"
@@ -188,7 +168,7 @@ const Login = () => {
         { /* === Connexion avec Google ou Twitter === */}
 
         <div className="justify-center flex w-full">
-            <SecondaryButton add="max-w-xs w-full flex flex-row items-center justify-center" action={() => { handleProviderLogin("google") }}>
+            <SecondaryButton add="max-w-xs w-full flex flex-row items-center justify-center">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512" className="fill-indigo-600 h-5 mr-4 text-white">
                     <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
                 </svg>
@@ -197,7 +177,7 @@ const Login = () => {
         </div>
 
         { false && <div className="justify-center flex w-full">
-            <SecondaryButton add="max-w-xs w-full flex flex-row items-center justify-center" action={() => { handleProviderLogin("twitter") }}>
+            <SecondaryButton add="max-w-xs w-full flex flex-row items-center justify-center">
                 <svg xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve" viewBox="0 0 248 204" className="fill-indigo-600 h-5 mr-4 text-white">
                     <path d="m222 51.3.1 6.5c0 66.8-50.8 143.7-143.7 143.7A143 143 0 0 1 1 178.8a101.4 101.4 0 0 0 74.7-21 50.6 50.6 0 0 1-47.1-35 50.3 50.3 0 0 0 22.8-.8 50.5 50.5 0 0 1-40.5-49.5v-.7c7 4 14.8 6.1 22.9 6.3A50.6 50.6 0 0 1 18 10.7a143.3 143.3 0 0 0 104.1 52.8 50.5 50.5 0 0 1 86-46c11.4-2.3 22.2-6.5 32.1-12.3A50.7 50.7 0 0 1 218.1 33c10-1.2 19.8-3.9 29-8-6.7 10.2-15.3 19-25.2 26.2z" />
                 </svg>
