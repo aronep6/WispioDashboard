aaa1af071ba3232ff46792238389e293539d9d5e
Ousmane Mbaye <mbayeousmane900@gmail.com>
2023-03-08 18:28:34 +0100

    feat(atomic): Atomic titles elements corrections

--- src/app_atomic/Title.tsx
+++ src/app_atomic/Title.tsx
@@ -1,25 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const PrimaryTitle = ({ children, add = "" }: { children: React.ReactNode, add: string }) => {
-    return <h1 className={`grotesk dark:text-white tracking-tighter py-2.5 px-1 leading-6 font-bold text-xl md:text-2xl ${add}`}> {/*text-slate-900*/}
+const PrimaryTitle = ({ children, add = "" }: { children: React.ReactNode | string, add?: string }) => {
+    return <h1 className={`grotesk dark:text-white tracking-tighter py-2.5 px-0.5 leading-6 font-bold text-xl md:text-2xl ${add}`}> {/*text-slate-900*/}
         {children}
     </h1>
 };
 
-const SecondaryTitle = ({ children, add = "" }: { children: React.ReactNode, add: string }) => {
-    return <h2 className={`grotesk dark:text-white flex flex-row items-center tracking-tighter py-2.5 px-1 leading-6 font-medium text-lg md:text-xl ${add}`}> {/*text-slate-600*/}
+const SecondaryTitle = ({ children, add = "" }: { children: React.ReactNode | string, add?: string }) => {
+    return <h2 className={`grotesk dark:text-white flex flex-row items-center tracking-tighter py-2.5 px-0.5 leading-6 font-medium text-lg md:text-xl ${add}`}> {/*text-slate-600*/}
         {children}
     </h2>
 };
 
-const TertiaryTitle = ({ children, add = "" }: { children: React.ReactNode, add: string }) => {
-    return <h3 className={`grotesk dark:text-white tracking-tighter py-1.5 px-1 leading-5 font-semibold text-base md:text-lg ${add}`}> {/* text-slate-600 */}
+const TertiaryTitle = ({ children, add = "" }: { children: React.ReactNode | string, add?: string }) => {
+    return <h3 className={`grotesk dark:text-white tracking-tighter py-1.5 px-0.5 leading-5 font-semibold text-base md:text-lg ${add}`}> {/* text-slate-600 */}
         {children}
     </h3>
 };
 
-const FloatingTitle = ({ title, add = "" }: { title: string, add: string }) => {
+const FloatingTitle = ({ title, add = "" }: { title: string, add?: string }) => {
     return <div className={`floating-title absolute grotesk hidden bg-zinc-800 text-zinc-100 py-1.5 text-sm px-3 rounded-md group-hover:flex ${add}`}>
         <p className="whitespace-nowrap">{title}</p>
     </div>
@@ -27,18 +27,18 @@ const FloatingTitle = ({ title, add = "" }: { title: string, add: string }) => {
 
 const UserAt = ({ add, userId }: { add: string, userId: string }) => {
     return <Link to={`/app/profile/${userId}`} 
-        className={`text-blue-600 dark:text-blue-400 text-sm inter font-bold px-1 ${add}`}>
+        className={`text-blue-600 dark:text-blue-400 text-sm inter font-bold px-0.5 ${add}`}>
         @{ userId }
     </Link>
 };
 
-const BigTitle = ({ children, add = "" }: { children: React.ReactNode, add: string }) => {
-    return <h1 className={`dark:text-white tracking-tighter py-2.5 px-1 leading-6 font-bold text-3xl md:text-4xl ${add}`}> {/*text-slate-900*/}
+const BigTitle = ({ children, add = "" }: { children: React.ReactNode | string, add?: string }) => {
+    return <h1 className={`dark:text-white tracking-tighter py-2.5 px-0.5 leading-6 font-bold text-3xl md:text-4xl ${add}`}> {/*text-slate-900*/}
         {children}
     </h1>
 };
 
-const Hint = ({ children, add = "" }: { children: React.ReactNode, add?: string }) => {
+const Hint = ({ children, add = "" }: { children: React.ReactNode | string, add?: string }) => {
     return <span className={`text-xs text-gray-600 dark:text-gray-400 leading-snug mx-0.5 ${ add }`}>{ children }</span>;
 };
 
