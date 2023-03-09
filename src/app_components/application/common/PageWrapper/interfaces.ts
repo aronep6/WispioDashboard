ff573fc89107c18fa280f2a99fe79c12c83ec9a4
Ousmane Mbaye <mbayeousmane900@gmail.com>
2023-03-09 15:12:05 +0100

    feat(pageWrapper): Page wrapper updated for supporting error displaying and extended titles

--- src/app_components/application/common/PageWrapper/interfaces.ts
+++ src/app_components/application/common/PageWrapper/interfaces.ts
@@ -2,4 +2,6 @@ export interface PageWrapperProps {
     pageTitle: string;
     children: React.ReactNode;
     isLoading?: boolean;
+    error?: string | null | undefined | Error;
+    extendedTitle?: string | undefined;
 }
