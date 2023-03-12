a6531cf45b0d87f2800ea7550070550b41605fe5
Ousmane Mbaye <mbayeousmane900@gmail.com>
2023-03-12 23:16:16 +0100

    feat(editorUseParams): useParams for projectId scope logic moved from context to parent component

--- src/app_components/application/Editor/index.tsx
+++ src/app_components/application/Editor/index.tsx
@@ -1,24 +1,31 @@
-import { Outlet } from "react-router-dom";
+import { memo } from "react";
+import { Outlet, useParams } from "react-router-dom";
 import { EditorProvider } from "../../../app_contexts/Editor";
 import { EditorServiceProvider } from "../../../app_contexts/EditorService";
+import { type ProjectId } from "../common/interfaces/Editor";
 import PageWrapper from "../common/PageWrapper";
 
 const pageProps = {
     pageTitle: "Editor",
+    usePadding: false,
 };
 
 const Editor = () => {
     return <PageWrapper {...pageProps}>
-        <div className="flex flex-col">
+        <div className="flex flex-col h-full">
             <Outlet />
         </div>
     </PageWrapper>;
 };
 
-export default function index() {
+function index() {
+    const projectId: ProjectId = useParams<{ projectId: ProjectId }>().projectId;
+
     return <EditorServiceProvider>
-        <EditorProvider>
+        <EditorProvider projectId={projectId}>
             <Editor />
         </EditorProvider>
     </EditorServiceProvider>
 };
+
+export default memo(index, () => false);
