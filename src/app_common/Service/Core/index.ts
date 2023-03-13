05eeee5182f2e8b39fb3f549521c25af37ae1681
Ousmane Mbaye <mbayeousmane900@gmail.com>
2023-03-13 17:12:06 +0100

    feat(CoreService): New methods added for query collections and documents in database

--- src/app_common/Service/Core/index.ts
+++ src/app_common/Service/Core/index.ts
@@ -1,14 +1,17 @@
 import { initializeApp } from "firebase/app";
 
-import type { Firestore } from "firebase/firestore";
-import { initializeFirestore } from "firebase/firestore";
+import type { Firestore, DocumentData } from "firebase/firestore";
+import { initializeFirestore, collection, doc, getDoc, getDocs } from "firebase/firestore";
 
 import type { Auth } from "firebase/auth";
 import { getAuth } from "firebase/auth";
 
-import { 
-    firebaseDatabaseConfiguration, 
-    FirebaseServiceConfiguration
+import {
+    firebaseDatabaseConfiguration,
+    FirebaseRootCollection,
+    MultipleDocumentsResponse,
+    type FirebaseServiceConfiguration,
+    type UserAccessibleCollection
 } from "./interfaces";
 
 const service_config: FirebaseServiceConfiguration = {
@@ -40,6 +43,56 @@ class Core {
         this.db = db;
         this.auth = auth;
         this.sleep = sleep;
+    }
+
+    getCurrentUserID = () => {
+        return this.auth.currentUser?.uid as string;
+    }
+
+    getDocument = async (
+        collection: UserAccessibleCollection,
+        document: string
+    ): Promise<DocumentData> => {
+        try {
+            const userId = this.getCurrentUserID();
+
+            if (!userId) throw new Error("User is not logged in!");
+
+            const docRef = doc(this.db, FirebaseRootCollection, userId, collection, document);
+            const docSnap = await getDoc(docRef);
+
+            if (docSnap.exists()) {
+                return docSnap.data();
+            } else {
+                throw new Error("The target document does not exist, or you're not authorized to access it.");
+            }
+
+        } catch (error: any) {
+            throw new Error(error.message);
+        }
+    }
+    
+    getMultipleDocuments = async (
+        target_collection: UserAccessibleCollection,
+    ): Promise<MultipleDocumentsResponse[]> => {
+        try {
+            const userId = this.getCurrentUserID();
+
+            if (!userId) throw new Error("User is not logged in!");
+
+
+            const collectionRef = collection(this.db, FirebaseRootCollection, userId, target_collection);
+            const docSnap = await getDocs(collectionRef);
+
+            return docSnap.docs.map((doc) => {
+                return {
+                    id: doc.id,
+                    data: doc.data(),
+                }
+            });
+        } catch (error: any) {
+            throw new Error(error.message);
+        }
     }
 }
 
