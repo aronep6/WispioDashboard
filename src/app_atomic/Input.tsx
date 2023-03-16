d8104688aa952eab21699cbf75d2bea022fa3297
Ousmane Mbaye <mbayeousmane900@gmail.com>
2023-03-16 12:48:31 +0100

    feat(atomic): New atomic component for textarea

--- src/app_atomic/Input.tsx
+++ src/app_atomic/Input.tsx
@@ -4,7 +4,7 @@ import { FieldError } from "react-hook-form/dist/types";
 interface InputProps {
     name: string;
     label?: string;
-    value: string;
+    value: string | number;
     add?: string;
     type?: string;
     required?: boolean;
@@ -13,6 +13,11 @@ interface InputProps {
     placeholder?: string;
     error?: FieldError | boolean;
     errorMessage?: string;
+}
+
+interface InputAreaPropsExtension {
+    rows?: number;
+    cols?: number;
 }
 
 const InputBlock = forwardRef(({
@@ -65,4 +70,54 @@ ref: React.Ref<HTMLInputElement> | undefined
     </section>
 });
 
-export { InputBlock };
+const InputBlockArea = forwardRef(({
+    name,
+    label,
+    value,
+    add="",
+    required = false,
+    onChange = () => {},
+    disabled = false,
+    placeholder = "Entrez une valeur",
+    error = false,
+    errorMessage = "Une erreur est survenue",
+    rows = 4,
+}: InputProps & InputAreaPropsExtension,
+ref: React.Ref<HTMLTextAreaElement> | undefined
+) => {
+    return <section className="flex flex-col py-1 my-0.5 w-full">
+
+        { label && <label className="text-slate-700 mb-1.5 ml-0.5 text-sm font-medium tracking-tight">{ label }</label> }
+
+        <textarea
+            ref={ref}
+            rows={rows}
+            className={`
+                relative
+                bg-white shadow-sm
+                ${add}
+                text-slate-900
+                border-slate-300 ${ error ? "border-red-500" : "border" }
+                border border-solid box-border
+                focus:border-indigo-500 focus:shadow-outline-indigo
+                focus:ring focus:ring-indigo-400 duration-100
+                focus:outline-none
+                rounded-lg
+                font-medium placeholder:text-sm
+                placeholder-slate-400
+                ${disabled && "cursor-not-allowed"}
+                text-base p-3.5`}
+            value={value}
+            spellCheck={false}
+            onChange={(e) => { onChange(e.target.value) }}
+            disabled={disabled}
+            required={required}
+            placeholder={placeholder}
+        />
+        {
+            error && <span className="text-red-500 text-xs font-medium mt-0.5">{ errorMessage }</span>
+        }
+    </section>
+});
+
+export { InputBlock, InputBlockArea };
