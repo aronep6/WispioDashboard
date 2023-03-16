c246c54f64f81cd4e3a84af4c8c999cae84d6d9a
Ousmane Mbaye <mbayeousmane900@gmail.com>
2023-03-16 20:32:02 +0100

    feat(Dashboard): Added page generic poster for the dashboard page

--- src/app_components/application/Dashboard/index.tsx
+++ src/app_components/application/Dashboard/index.tsx
@@ -1,4 +1,7 @@
+import { Grid } from "react-feather";
+import PageGenericPoster from "../../PagePoster/PageGenericDisplay";
 import PageWrapper from "../common/PageWrapper";
+import { Link } from "react-router-dom";
 
 const pageProps = {
     pageTitle: "Tableau de bord",
@@ -6,9 +9,17 @@ const pageProps = {
 
 const Dashboard = () => {
     return <PageWrapper {...pageProps}>
-        <div className="flex flex-col">
-            This is the dashboard page content area (not the sidebar)
-        </div>
+        <PageGenericPoster
+            icon={{
+                type: Grid
+            }}
+            title="Bienvenue sur Wispio"
+            message={
+                <span>
+                    Votre tableau de bord est encore vide pour le moment, vous pourrez y suivre l'avancement de vos tâches de transcription en temps réel. Vous pouvez commencer par créer une nouvelle transcription ou consulter vos <Link to="/tasks" className="text-indigo-600 font-medium">tâches actives</Link>.
+                </span>
+            }
+        />
     </PageWrapper>;
 }
 
