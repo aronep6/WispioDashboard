8039219c685282a4a6c1ec027bb9d2eee8cfff04
Ousmane Mbaye <mbayeousmane900@gmail.com>
2023-03-19 04:31:55 +0100

    feat(date-fns): Date displaying in 'fr' locale for the fn dateFromNow

--- src/app_common/functions/date-from-now.ts
+++ src/app_common/functions/date-from-now.ts
@@ -1,7 +1,11 @@
 import { formatDistanceToNow } from 'date-fns';
+import { fr } from 'date-fns/locale';
 
 const dateFromNow = (date: Date): string => {
-    return formatDistanceToNow(date, { addSuffix: true });
+    return formatDistanceToNow(date, { 
+        addSuffix: true,
+        locale: fr,
+    });
 };
 
 export default dateFromNow;
