9a6d9795bf8186392595af694d641ce3f991204d
Ousmane Mbaye <mbayeousmane900@gmail.com>
2023-03-22 00:23:03 +0100

    feat(SingleSettingPageWrapper): Style updated

--- src/app_components/application/AccountSettings/components/SingleSettingPageWrapper/index.tsx
+++ src/app_components/application/AccountSettings/components/SingleSettingPageWrapper/index.tsx
@@ -12,15 +12,17 @@ const SingleSettingPageWrapper = ({
   useWebTitle(`${title} - Paramètres - ${ import.meta.env.VITE_APPLICATION_NAME }`);
 
   return (
-    <div className="flex flex-col w-full h-full p-4 opening-page-wrapper gap-2">
-      <Card add="flex flex-col gap-1">
-        <SecondaryTitle>
-          { title }
-        </SecondaryTitle>
-        <p className="text-sm text-slate-500">
-          { description }
-        </p>
-      </Card>
+    <div className="flex flex-col w-full h-full p-4 pt-0 opening-page-wrapper gap-2 overflow-y-auto">
+      <div className="sticky top-0 z-10 pt-4 backdrop-filter backdrop-blur-sm">
+        <Card add="flex flex-col gap-1 shrink-0 shadow-lg shadow-slate-200 border-slate-400">
+          <SecondaryTitle>
+            { title }
+          </SecondaryTitle>
+          <p className="text-sm text-slate-500">
+            { description }
+          </p>
+        </Card>
+      </div>
       <div className="flex flex-col gap-2">
         {
           children
