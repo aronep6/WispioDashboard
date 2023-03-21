b7f74b9f37c77cb461591b543bf4dff9c6d1c059
Ousmane Mbaye <mbayeousmane900@gmail.com>
2023-03-22 00:14:43 +0100

    feat(AccountSettingsService): Start implement update password method

--- src/app_common/Service/Application/AccountSettingsService/index.ts
+++ src/app_common/Service/Application/AccountSettingsService/index.ts
@@ -4,6 +4,19 @@ class AccountSettingsService extends Core {
     constructor() {
         super();
     }
+
+    // Security and privacy
+    _security_updatePassword = async (
+        password: string, 
+        password_retype: string
+    ) => {
+        try {
+            if (password !== password_retype) throw new Error('Password entries are different, please try again !');
+            throw new Error('Password update is not implemented yet !');
+        } catch (error: any) {
+            throw new Error(error.message);
+        }
+    }
 }
 
 export default AccountSettingsService;
