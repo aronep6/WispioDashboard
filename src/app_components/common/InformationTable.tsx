6423f1e78c0e8b11629c35bf63a825fea66c8dda
Ousmane Mbaye <mbayeousmane900@gmail.com>
2023-03-21 23:58:16 +0100

    feat(InformationTable): lastIdx border logic added

--- src/app_components/common/InformationTable.tsx
+++ src/app_components/common/InformationTable.tsx
@@ -1,13 +1,15 @@
+import './styles.css';
 export interface SingleInformationTableRowInterface {
     key: string;
     value: any;
 }
 
 const InformationTable = ({ table }: { table: SingleInformationTableRowInterface[] }) => {
-    return <table className="w-full font-medium">
+    const lastIdx = table.length - 1;
+    return <table className="w-full font-medium information-table">
         <tbody>
             {table.map((row, index) => {
-                return <tr key={index} className="border-b border-gray-200 hover:bg-slate-200">
+                return <tr key={index} className={`${ lastIdx !== index && "border-b"} border-gray-200 hover:bg-slate-200`}>
                     <td className="py-2.5 px-1 text-left text-slate-600 text-sm"> {row.key} </td>
                     <td className="py-2.5 px-1 text-left text-sm"> {row.value} </td>
                 </tr>
