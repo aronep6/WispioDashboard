2f754c1c085c1806de7931522dbb6a68e790c025
Ousmane Mbaye <mbayeousmane900@gmail.com>
2023-03-30 20:07:46 +0200

    feat(ApplicationUI): Modal react portal logic created + ModalSection created

--- src/app_atomic/Modal.tsx
+++ src/app_atomic/Modal.tsx
@@ -1,4 +1,7 @@
 import { Fragment } from "react";
+import { createPortal } from "react-dom";
+import { Hint, SecondaryTitle } from "./Title";
+const modalRoot = document.getElementById('modal-root') as HTMLElement;
 
 interface ModalProps {
     children: JSX.Element | JSX.Element[];
@@ -9,26 +12,28 @@ interface ModalHrProps {
     color?: string;
 }
 
-const Modal = ({ 
-    children, 
-    onClose = () => {},
+const Modal = ({
+    children,
+    onClose = () => { },
 }: ModalProps) => {
-    return <Fragment>
-        <div className="fixed inset-0 bg-slate-900 opacity-75 z-40"></div>
-        <ModalContainer onClose={onClose}>
-            { children }
-        </ModalContainer>
-    </Fragment>
+    return createPortal(
+        <Fragment>
+            <div className="fixed inset-0 bg-slate-900 opacity-75 z-40"></div>
+            <ModalContainer onClose={onClose}>
+                {children}
+            </ModalContainer>
+        </Fragment>
+    , modalRoot);
 };
 
-const ModalContainer = ({ 
-    children, 
+const ModalContainer = ({
+    children,
     onClose,
 }: ModalProps) => {
     return <div onClick={onClose}
-        className="fixed h-screen modal-bg-animation w-screen backdrop-blur-sm inset-0 modal-bg-animation z-50 flex items-center justify-center">
+        className="fixed inter h-screen modal-bg-animation w-screen backdrop-blur-sm inset-0 modal-bg-animation z-50 flex items-center justify-center">
         <section className="overflow-hidden m-2 duration-150 modal-animation" onClick={e => e.stopPropagation()}>
-            { children }
+            {children}
         </section>
     </div>
 };
@@ -36,7 +41,16 @@ const ModalContainer = ({
 const ModalHr = ({
     color
 }: ModalHrProps) => {
-    return <hr className={`-mx-20 my-2.5 ${color}`} /> 
+    return <hr className={`-mx-20 my-2.5 ${color}`} />
 };
 
-export { Modal, ModalHr };
+const ModalSection = ({ title, children }: { title: string, children: React.ReactNode }) => {
+    return <div className='flex flex-col px-2 gap-2'>
+        <SecondaryTitle>{ title }</SecondaryTitle>
+        <div className='flex flex-col gap-2'>
+            {children}
+        </div>
+    </div>
+};
+
+export { Modal, ModalHr, ModalSection };
