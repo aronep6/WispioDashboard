5f19026eaa7b24a48d3731431e936b464bc0086a
Ousmane Mbaye <mbayeousmane900@gmail.com>
2023-04-27 21:39:16 +0200

    feat(SnackbarProvider): Placed SnackbarProvider at the root of the application

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { AuthenticationSessionProvider } from './app_contexts/AuthenticationSession'
 import './index.css'
+import App from './App'
+import ReactDOM from 'react-dom/client'
+import { AuthenticationSessionProvider } from './app_contexts/AuthenticationSession'
+import { SnackbarServiceProvider } from './app_contexts/SnackbarService'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <AuthenticationSessionProvider>
-    <App />
-  </AuthenticationSessionProvider>
+  <SnackbarServiceProvider>
+    <AuthenticationSessionProvider>
+      <App />
+    </AuthenticationSessionProvider>
+  </SnackbarServiceProvider>
 )
