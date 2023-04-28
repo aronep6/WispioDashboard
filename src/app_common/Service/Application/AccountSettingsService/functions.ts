44c249e7c6bc93bb4d214105ad8b59ef071c854f
Ousmane Mbaye <mbayeousmane900@gmail.com>
2023-04-28 09:52:10 +0200

    feat(PasswordUpdate): getFirebaseError function improved

--- src/app_common/Service/Application/AccountSettingsService/functions.ts
+++ src/app_common/Service/Application/AccountSettingsService/functions.ts
@@ -1,6 +1,7 @@
-const getFirebaseError = (e: string): string => {
+const getFirebaseError = (error: any): string => {
+    const e = error?.message?.toLowerCase() || '';
     if (e.includes('auth/requires-recent-login')) return 'La modification du mot de passe requiert une authentification récente. Reconnectez-vous et réessayez.';
-    return 'Une erreur inconnue est survenue';
+    return 'Une erreur inconnue est survenue, veuillez réessayer ultérieurement.';
 };
 
 export default getFirebaseError;
