c67bb26b7f3a0a4eeadceafee6e505c994509d66
Ousmane Mbaye <mbayeousmane900@gmail.com>
2023-05-06 23:27:26 +0200

    feat(Files): Files fixtures updated

--- src/app_common/Service/Application/FilesService/files.fixture.ts
+++ src/app_common/Service/Application/FilesService/files.fixture.ts
@@ -2,7 +2,7 @@ import { ApplicationFile } from "./interfaces";
 
 const _file_1_: ApplicationFile = {
     id: "1",
-    name: "file_1",
+    name: "video_test",
     bucket: "bucket_1",
     path: "path_1",
     size: 8956100,
@@ -19,6 +19,7 @@ const _file_1_: ApplicationFile = {
     },
     owner: {
         uid: "1",
+        displayName: "Ousmane Mbaye"
     },
     contentTypes: [
         "image/jpeg",
@@ -28,7 +29,7 @@ const _file_1_: ApplicationFile = {
 
 const _file_2_: ApplicationFile = {
     id: "2",
-    name: "file_2",
+    name: "video_pour_hagar",
     bucket: "bucket_1",
     path: "path_2",
     size: 1469300,
@@ -45,6 +46,7 @@ const _file_2_: ApplicationFile = {
     },
     owner: {
         uid: "1",
+        displayName: "Ousmane Mbaye"
     },
     contentTypes: [
         "audio/mpeg",
