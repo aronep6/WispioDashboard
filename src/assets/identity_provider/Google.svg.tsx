5573981f2290bbe9f23354c04c88063483c4d320
Ousmane Mbaye <mbayeousmane900@gmail.com>
2023-05-25 15:43:03 +0200

    feat(assets): Google, Microsoft and GitHub svg components added

---
+++ src/assets/identity_provider/Google.svg.tsx
@@ -0,0 +1,21 @@
+import type { SVGProps } from "react"
+
+const Google = (props: SVGProps<SVGSVGElement>) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 186.7 190.5" {...props}>
+        <path
+            fill="#4285f4"
+            d="M95 78v37h52c-3 12-9 22-20 28l31 24c18-16 29-41 29-70l-2-19z"
+        />
+        <path
+            fill="#34a853"
+            d="m42 113-7 6-25 19c16 31 48 53 85 53 26 0 48-9 63-24l-31-24c-8 6-19 10-32 10-25 0-46-17-53-40z"
+        />
+        <path fill="#fbbc05" d="M10 53a94 94 0 0 0 0 85l32-25a57 57 0 0 1 0-36z" />
+        <path
+            fill="#ea4335"
+            d="M95 38c14 0 27 5 37 14l27-27A95 95 0 0 0 10 53l32 24c7-22 28-39 53-39z"
+        />
+    </svg>
+);
+
+export default Google;
