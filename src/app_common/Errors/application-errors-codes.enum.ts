8d8de9166ec23d484fc953435f664dfea1c8d36e
Ousmane Mbaye <mbayeousmane900@gmail.com>
2023-05-28 14:31:49 +0200

    fix(ApplicationError): Updated enum value for passing typescript check

--- src/app_common/Errors/application-errors-codes.enum.ts
+++ src/app_common/Errors/application-errors-codes.enum.ts
@@ -1,5 +1,5 @@
 export const APP_ERROR_CODE_PREFIX = 'app/error/';
 
 export enum APPLICATION_ERRORS_CODES {
-    UNAVAILABLE_IDENTITY_PROVIDER = `${ APP_ERROR_CODE_PREFIX }unavailable_identity_provider`
+    UNAVAILABLE_IDENTITY_PROVIDER = 'app/error/unavailable_identity_provider'
 }
