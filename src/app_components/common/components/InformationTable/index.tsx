bfc0fc8e1351d11d0e05fbd78f379d91467b0b59
Ousmane Mbaye <mbayeousmane900@gmail.com>
2023-06-15 14:40:42 +0200

    feat(information-table): Updated component for the subRow feature

--- src/app_components/common/components/InformationTable/index.tsx
+++ src/app_components/common/components/InformationTable/index.tsx
@@ -1,14 +1,30 @@
+import { Fragment } from 'react';
 import { SingleInformationTableRowInterface } from './interfaces';
 import './styles.css';
+
+const Row = ({ row, }: { row: SingleInformationTableRowInterface }) => {
+    const { key, value } = row;
+    return <Fragment>
+        <td className="py-2.5 px-1 text-left text-slate-600 text-sm"> {key} </td>
+        <td className="py-2.5 px-1 text-left text-sm"> {value} </td>
+    </Fragment>
+}
+
+const SubRow = ({ row }: { row: SingleInformationTableRowInterface }) => {
+    const { key, value } = row;
+    return <Fragment>
+        <td className="py-2.5 px-2 text-left bg-slate-100 text-slate-600 text-sm"> ↳ {key} </td>
+        <td className="py-2.5 px-1 bg-slate-100 text-left text-sm"> {value} </td>
+    </Fragment>
+}
 
 const InformationTable = ({ table }: { table: SingleInformationTableRowInterface[] }) => {
     const lastIdx = table.length - 1;
     return <table className="w-full font-medium information-table">
         <tbody>
             {table.map((row, index) => {
-                return <tr key={index} className={`${ lastIdx !== index && "border-b"} border-gray-200 hover:bg-slate-200`}>
-                    <td className="py-2.5 px-1 text-left text-slate-600 text-sm"> {row.key} </td>
-                    <td className="py-2.5 px-1 text-left text-sm"> {row.value} </td>
+                return <tr key={index} className={`${lastIdx !== index && "border-b"} border-gray-200 hover:bg-slate-200`}>
+                    {row.isSubRow ? <SubRow row={row} /> : <Row row={row} />}
                 </tr>
             })}
         </tbody>
--- src/app_components/common/components/InformationTable/interfaces.ts
+++ src/app_components/common/components/InformationTable/interfaces.ts
@@ -1,4 +1,5 @@
 export interface SingleInformationTableRowInterface {
     key: string;
     value: any;
+    isSubRow?: boolean;
 }
